c1045bfd3f602494d172d0c85166f06f41b4dd6a
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-05-16 19:37:49 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/example/gitlabcidockerexample/GitlabCiDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example.gitlabcidockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCiDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabCiDockerExampleApplication.class, args);
+	}
+
+}
